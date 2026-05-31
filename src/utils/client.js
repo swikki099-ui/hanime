17ce3d95baef2c86e8c7d9ced1f4f163d91bed70
swikki099-ui <swikki099@gmail.com>
2026-06-01 01:51:50 +0530

    Update client.js

--- src/utils/client.js
+++ src/utils/client.js
@@ -4,5 +4,12 @@ import {config} from 'dotenv'
 config()
 
 export const client = axios.create({
-  baseURL: 'https://watchhentai.net'
+  baseURL: 'https://watchhentai.net',
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Referer': 'https://watchhentai.net/',
+    'Connection': 'keep-alive'
+  }
 })
