b937585a20921044b0426afc5e731f2f513581c2
swikki099-ui <swikki099@gmail.com>
2026-05-31 20:50:06 +0530

    Add files via upload

---
+++ src/routers/index.js
@@ -0,0 +1,33 @@
+import { Router } from "express";
+import { getSpotlight } from "../controllers/spotlight.js";
+import { getCategory } from "../controllers/category.js";
+import { getSearch } from "../controllers/search.js";
+import { getRecentEpisodes } from "../controllers/recentEpisodes.js";
+import { getInfo } from "../controllers/info.js";
+import { getRelated } from "../controllers/related.js";
+import { getEpisodes } from "../controllers/episodes.js";
+import { getSource } from "../controllers/source.js";
+import { mp4Proxy } from "../proxy/mp4Proxy.js";
+import { downloadMp4 } from "../controllers/downloadMp4.js";
+
+export const router = Router();
+
+router.get("/download-mp4", downloadMp4);
+
+router.get('/mp4-proxy', mp4Proxy)
+
+router.get('/spotlight', getSpotlight)
+
+router.get('/recent-episodes', getRecentEpisodes)
+
+router.get('/category/:category{/:type}', getCategory)
+
+router.get('/search', getSearch)
+
+router.get('/related/:id', getRelated)
+
+router.get('/info/:id', getInfo)
+
+router.get('/episodes/:id', getEpisodes)
+
+router.get('/source/:episodeId', getSource)
